f8874648a73808af2f68caa49ddc06fe0635c675
Daviez71 <agbebidavid538@gmail.com>
2026-05-25 13:38:23 +0100

    last editing of minor things

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -211,7 +211,7 @@ function RecipeDetails() {
           <option value="">Select a day</option>
           <option value="Monday">Monday</option>
           <option value="Tuesday">Tuesday</option>
-          <option value="Wednesday">Wedneeday</option>
+          <option value="Wednesday">Wednesday</option>
           <option value="Thursday">Thursday</option>
           <option value="Friday">Friday</option>
           <option value="Saturday">Saturday</option>
